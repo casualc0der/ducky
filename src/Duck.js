11cfc2222108efb37a40c36002ef762da465bc03
Edd Sansome <edwardsansome@gmail.com>
2020-07-14 23:03:18 +0100

    v1

---
+++ src/Duck.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import {Container} from 'reactbulma'
+import DebugForm from './DebugForm.js'
+
+
+const Duck =() => {
+
+
+return(
+
+<div className="columns is-centered is-mobile">
+  <div className="column has-text-centered">
+ 
+<DebugForm />
+
+
+
+
+
+</div>
+</div>
+
+
+)
+
+}
+
+
+
+
+
+export default Duck
---
+++ src/Icons/index.js
@@ -0,0 +1,1 @@
+export { default as RubberDuck } from './RubberDuck'
