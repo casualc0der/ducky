b9daa50be74af2dad437b785b3c80be5f3cdc70e
Edd Sansome <edwardsansome@gmail.com>
2020-07-14 23:12:02 +0100

    Updates

---
+++ precache-manifest.5b70af4d93ac37ae3e76586f4047e305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6255c9f00c4b419b83aca424f4f400bc",
+    "url": "/ducky/index.html"
+  },
+  {
+    "revision": "0071d877818a2dd16185",
+    "url": "/ducky/static/css/2.1d984fbf.chunk.css"
+  },
+  {
+    "revision": "0071d877818a2dd16185",
+    "url": "/ducky/static/js/2.e35aad97.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ducky/static/js/2.e35aad97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810334ab0779fc801820",
+    "url": "/ducky/static/js/main.b2cfa87e.chunk.js"
+  },
+  {
+    "revision": "17f0e1c9b7dd11425e8a",
+    "url": "/ducky/static/js/runtime-main.ffe6f35b.js"
+  }
+]);
